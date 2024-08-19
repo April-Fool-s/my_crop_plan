7a278f8e559563acf991d9b67ffeb90fbae25c08
3rarara <161809435+3rarara@users.noreply.github.com>
2024-08-19 08:50:48 +0000

    feature/guest_sign_in:ゲストログイン機能実装のため

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
 
   # アソシエーション
   has_many :fields, dependent: :destroy
+
+  GUEST_USER_EMAIL = "guest@example.com"
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+    end
+  end
+
+  def guest_user?
+    email == GUEST_USER_EMAIL
+  end
+
 end
