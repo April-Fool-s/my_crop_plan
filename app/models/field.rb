049763b1f147a61da301643ce5412009b124228a
3rarara <161809435+3rarara@users.noreply.github.com>
2024-08-22 08:13:03 +0000

    feature/footer:フッターアイコン作成

--- app/models/field.rb
+++ app/models/field.rb
@@ -5,6 +5,6 @@ class Field < ApplicationRecord
   has_many :field_sections, dependent: :destroy
   has_many :plans, dependent: :destroy
 
-  # 子モデル（plan_details）の属性を受入れ、更新や削除を許可する
+  # 子モデル（field_sections）の属性を受入れ、更新や削除を許可する
   accepts_nested_attributes_for :field_sections, allow_destroy: true
 end
